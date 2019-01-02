fd90a85678de33621adcf7b6b1ae4fc3613bfd41
phicdy <phicdy@gmail.com>
2019-01-02 17:10:55 +0900

    Search: Set up SearchView

--- app/src/main/java/com/phicdy/advancedkeywordsearch/SettingActivity.kt
+++ app/src/main/java/com/phicdy/advancedkeywordsearch/SettingActivity.kt
@@ -1,12 +1,50 @@
 package com.phicdy.advancedkeywordsearch
 
+import android.app.SearchManager
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
+import android.widget.Toast
+
 
 class SettingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_setting, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    clearFocus()
+                    try {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=$query"))
+                        startActivity(intent)
+                    } catch (e: ActivityNotFoundException) {
+                        Toast.makeText(this@SettingActivity, getString(R.string.error_activity_not_found), Toast.LENGTH_SHORT).show()
+                    }
+                    return true // Handled the event in the listener, not handle in default SearchView
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return true // Handled the event in the listener, not handle in default SearchView
+                }
+
+            })
+        }
+
+        return true
     }
 }
