ec8c78bda07a39e0b5fcd9d7fdf67b62cfe9d1b4
phicdy <phicdy@gmail.com>
2019-05-09 20:59:27 +0900

    Project: Add Stetho

--- app/src/main/java/com/phicdy/advancedkeywordsearch/AdvancedKeyWordSearchApplication.kt
+++ app/src/main/java/com/phicdy/advancedkeywordsearch/AdvancedKeyWordSearchApplication.kt
@@ -1,5 +1,6 @@
 package com.phicdy.advancedkeywordsearch
 
+import com.facebook.stetho.Stetho
 import com.phicdy.advancedkeywordsearch.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
@@ -7,4 +8,16 @@ import dagger.android.support.DaggerApplication
 class AdvancedKeyWordSearchApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
         DaggerAppComponent.builder().create(this)
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Stetho.initialize(
+                Stetho.newInitializerBuilder(this)
+                    .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                    .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                    .build()
+            )
+        }
+    }
 }
