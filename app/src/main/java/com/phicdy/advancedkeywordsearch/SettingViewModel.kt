8e4bc072b9b8b5a56e779774d73b4e5eae094f07
phicdy <phicdy@gmail.com>
2019-10-09 20:13:50 +0900

    Refactor: Delete unused methods

--- app/src/main/java/com/phicdy/advancedkeywordsearch/SettingViewModel.kt
+++ app/src/main/java/com/phicdy/advancedkeywordsearch/SettingViewModel.kt
@@ -6,14 +6,11 @@ import androidx.lifecycle.viewModelScope
 import com.phicdy.advancedkeywordsearch.domain.entity.Period
 import com.phicdy.advancedkeywordsearch.domain.usecase.SearchUrlOptionUseCase
 import com.phicdy.advancedkeywordsearch.model.ExcludedKeyword
-import com.phicdy.advancedkeywordsearch.model.SearchSetting
 import com.phicdy.advancedkeywordsearch.model.SearchSettingAndKeywords
 import com.phicdy.advancedkeywordsearch.repository.SettingRepository
 import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SettingViewModel @Inject constructor(
@@ -23,26 +20,6 @@ class SettingViewModel @Inject constructor(
 
     val searchSetting: Deferred<LiveData<SearchSettingAndKeywords>> by lazy {
         viewModelScope.async(start = CoroutineStart.LAZY) { settingRepository.fetch() }
-    }
-
-    suspend fun changeDefault(
-        tappedId: Long,
-        currentList: MutableList<SearchSettingAndKeywords>
-    ) = withContext(Dispatchers.IO) {
-        val updatedList = mutableListOf<SearchSetting>()
-        currentList.forEach {
-            if (it.setting.id == tappedId) {
-                if (it.setting.defaultEnabled) return@withContext // No change
-                updatedList.add(it.setting.copy(defaultEnabled = true))
-            } else {
-                updatedList.add(it.setting.copy(defaultEnabled = false))
-            }
-        }
-        settingRepository.update(updatedList)
-    }
-
-    suspend fun delete(setting: SearchSetting) {
-        settingRepository.delete(setting)
     }
 
     fun generateSearchUrlOption(
